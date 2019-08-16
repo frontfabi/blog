e32dcba040ec7676b23a8bf0030d848d3899ac05
Fabi <fabiana.nascimento@terceiro-sky.com.br>
2019-08-16 18:00:53 -0300

    changing colors - functional approach

--- themes/fronthub/static/js/randomColors.js
+++ themes/fronthub/static/js/randomColors.js
@@ -13,13 +13,19 @@ function getRandomColors() {
     "purple"
   ];
   const index = parseInt(Math.random() * (colors.length - 1 - 0 + 1), 10);
+  return colors[index];
+}
 
+function changeColor(color) {
   let classList = document.getElementsByClassName("nav-title")[0].classList;
   classList.forEach(classname => {
     if (typeof classname === "string" && classname !== "nav-title")
       classList.remove(classname);
   });
-  classList.add(colors[index]);
+  classList.add(color);
 }
 
-setInterval(getRandomColors, 2500);
+setInterval(() => {
+  var color = getRandomColors();
+  changeColor(color);
+}, 3000);
